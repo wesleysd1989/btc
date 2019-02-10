4382234f33c7aa23519ecfd61935a22a866f56e3
m3n0r <wesleysd1989@gmail.com>
2019-02-09 22:37:27 -0200

    Update index.js

--- index.js
+++ index.js
@@ -9,79 +9,44 @@ Sender Address>> mskVe45XC5pnsKd4xk1VEGyMJfbkCnGpZy   - key1
 Receiver Address>> mtwL3jZSSGK8gJ5DLNihccfePwFjibwjWV  - key2
 */
 
-//create send address 
+//create sender address
+var prvKeySenderWIF = 'cW96DX2rwipnH4KyF5GgvbjHTZzLyCrJm2RtCnksZ6QSTvyEQcL6';
+console.log(gerarPvtKey)
+var prvKeySender = bitcore.PrivateKey.fromWIF(prvKeySenderWIF);
+var addrSender = prvKeySender.toAddress();
+console.log('Sender Address>> ' + addrSender);
+
+//create receiver address
+var prvKeyReceiverWIF = 'cVn4yWbjDi2mxvaf1Q5jnjQiJsCmyzzYuUzRGGg7F2CiooYTgHdA';
+var prvKeyReceiver = bitcore.PrivateKey.fromWIF(prvKeyReceiverWIF);
+var addrReceiver = prvKeyReceiver.toAddress();
+console.log('Receiver Address>> ' + addrReceiver);
+
+var insight = new Insight('testnet');
+insight.getUtxos(addrSender, function (err, utxos) {
+    if (err) { }
+    else {
+        //Unspent transaction output for new transaction
+        console.log('Unspent Transaction OutPut ' + utxos)
+        var tx = bitcore.Transaction();
+        tx.from(utxos);
+        tx.to(addrReceiver, 100000000);
+        tx.change(addrSender);
+        tx.fee(50000);
+
+        //signing the transaction with sender`s private key
+        tx.sign(prvKeySender);
 
 
-function Gerar() {
-    var texto = "";
-    var tamanho = 52;
-    var caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        tx.serialize();
 
-    for (var i = 0; i < tamanho; i++)
-        texto += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-
-    return texto;
-}
-
-function allCombinations() {
-    var i = 0;
-    (function (i) {
-        setInterval(function () {
-            gerarPvtKey = Gerar()
-            try {
-                //create dender address
-                var prvKeySenderWIF = gerarPvtKey;
-                console.log(gerarPvtKey)
-                var prvKeySender = bitcore.PrivateKey.fromWIF(prvKeySenderWIF);
-                var addrSender = prvKeySender.toAddress();
-                console.log('Sender Address>> ' + addrSender);
-
-                //create receiver address
-                var prvKeyReceiverWIF = 'L1UXjw4Ya1qUPVe1EfhHwY3T6nUSGHdEZgAXYNTfUXTUfeVT1z4k'; // 17iSeFbBCj9567LCJNE4fqUmxcwC9B3Z7d
-                var prvKeyReceiver = bitcore.PrivateKey.fromWIF(prvKeyReceiverWIF);
-                var addrReceiver = prvKeyReceiver.toAddress();
-                console.log('Receiver Address>> ' + addrReceiver);
-
-                var insight = new Insight('mainnet');
-                insight.getUtxos(addrSender, function (err, utxos) {
-                    if (err) { }
-                    else {
-                        //Unspent transaction output for new transaction
-                        console.log('Unspent Transaction OutPut ' + utxos)
-                        var tx = bitcore.Transaction();
-                        tx.from(utxos);
-                        tx.to(addrReceiver, 100000000);
-                        tx.change(addrSender);
-                        tx.fee(50000);
-
-                        //signing the transaction with sender`s private key
-                        tx.sign(prvKeySender);
-
-
-                        tx.serialize();
-
-                        insight.broadcast(tx, function (err, returnedTxId) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            else {
-                                console.log('successful broadcast: ' + returnedTxId);
-                            }
-                        });
-                    }
-                });
+        insight.broadcast(tx, function (err, returnedTxId) {
+            if (err) {
+                console.log(err);
             }
-            catch (err) {
-                console.log('PrivateKey Invalida.')
+            else {
+                console.log('successful broadcast: ' + returnedTxId);
             }
-
-        }, 100)
-    })(i);
-}
-
-try {
-    allCombinations()
-}
-catch (err) {
-    console.log(err)
-}
+        });
+    }
+});
